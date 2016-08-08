94f036d103ad5e2a3394d380846dbee5f8c28f37
coderdreams <coderdreams@gmail.com>
2016-08-08 16:59:19 -0700

    Method should be private

--- trading/src/handler/RegisterTrader.hpp
+++ trading/src/handler/RegisterTrader.hpp
@@ -10,9 +10,9 @@ namespace handler {
 class RegisterTrader : public Poco::Net::HTTPRequestHandler
 {
 public:
-
     void handleRequest(Poco::Net::HTTPServerRequest& request,
                        Poco::Net::HTTPServerResponse& response);
+private:
     void saveUser(std::string username, std::string password);
 
 };
