25c7d48fc49c7ea06297e573efc404c536045f97
coderdreams <coderdreams@gmail.com>
2016-08-08 09:10:18 -0700

    Fixed dump

--- database/schema.sql
+++ database/schema.sql
@@ -2,6 +2,6 @@
 CREATE TABLE user (
   id INT unsigned NOT NULL auto_increment,
   name VARCHAR(255) COLLATE utf8mb4_unicode_ci,
-  pass VARCHAR(255) COLLATE utf8mb4_unicode_ci,,
+  pass VARCHAR(255) COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (id)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
