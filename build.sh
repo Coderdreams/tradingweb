dc711f9955b8df9b3b8a93e550d717cf0f2d1dda
coderdreams <coderdreams@gmail.com>
2020-11-16 12:36:51 -0300

    Change to generate cmake instead of premake since IDEs are much better working with the former, even when premake has a way more intuitive syntax.

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 conan install --build=missing -s compiler=gcc -s compiler.version=9 -s compiler.libcxx=libstdc++11 .
-premake5 gmake
+# Changing from premake to cmake because IDEs work better with it.
+#premake5 gmake
+mkdir -p build
+pushd build
+cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
 make
+popd
+source activate_run.sh
--- premake4.lua
+++ premake4.lua
@@ -1,6 +1,8 @@
 #!lua
 
 include("conanbuildinfo.premake.lua")
+--require 'conanpremake'
+--include("premake-cmake-master/cmake_cpp.lua")
 
 solution "TradingApp"
    configurations { "Debug", "Release" }
@@ -8,12 +10,12 @@ solution "TradingApp"
    libdirs { conan_libdirs }
    links { "mysqlcppconn", conan_libs }
    configuration { "linux", "gmake" }
-      buildoptions { "-Wall", "-std=c++2a" }
+      buildoptions { "-Wall", "-Wextra", "-Wpedantic", "-std=c++2a" }
    -- A project defines one build target
    project "tradingApp"
       kind "ConsoleApp"
       language "C++"
-      files { "trading/src/**.cpp" }
+      files { "trading/src/**.?pp" }
 
       configuration "Debug"
          defines { "DEBUG" }
